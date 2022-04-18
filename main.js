a9fbc0e53052f38f7710d2d5823225afdb5d1f26
kimhanlim97 <kimhanlim97@gmail.com>
2022-04-18 19:41:47 +0900

    저자 관련 CRUD 기능을 구현함

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import http from 'http'
 import url from 'url'
 
-import { detail, home } from './lib/show.js'
-import {create, createProcess} from './lib/create.js'
+import { detail, home } from './lib/topic.js'
+import { create, createProcess } from './lib/create.js'
 import { update, updateProcess } from './lib/update.js'
 import deleteProcess from './lib/delete.js'
+import { show, createAuthorProcess } from './lib/author.js'
+import { updateAuthor, updateAuthorProcess } from './lib/authorUpdate.js'
+import deleteAuthorProcess from './lib/authorDelete.js'
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -33,6 +36,21 @@ var app = http.createServer(function(request,response){
     }
     else if (pathName === "/delete_process") {
         deleteProcess(request, response);
+    }
+    else if (pathName === "/author") {
+        show(request, response);
+    }
+    else if (pathName === "/author/create_process") {
+        createAuthorProcess(request, response);
+    }
+    else if (pathName === "/author/update") {
+        updateAuthor(request, response, queryData);
+    }
+    else if (pathName === "/author/update_process") {
+        updateAuthorProcess(request, response);
+    }
+    else if (pathName === "/author/delete_process") {
+        deleteAuthorProcess(request, response);
     }
     else {
         response.writeHead(404);
