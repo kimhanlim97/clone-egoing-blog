4c3d456132b29eeb9ca5241a52428daab82f6b77
kimhanlim97 <kimhanlim97@gmail.com>
2022-04-20 16:21:38 +0900

    express를 활용해 블로그 topic read 기능과 create 기능을 구현함

--- node.mjs
+++ node.mjs
@@ -1,12 +1,29 @@
 import express from 'express'
 
+import { home, page } from './lib/topic.js'
+import { create, createProcess } from './lib/create.js'
+
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded());
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  home(res);
+});
+
+app.get('/page/:pageId', (req, res) => {
+  page(res, req.params)
+});
+
+app.get('/create', (req, res) => {
+  create(res)
+})
+
+app.post('/create', (req, res) => {
+  createProcess(res, req.body);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
