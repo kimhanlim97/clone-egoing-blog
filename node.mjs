849218e6afbb8b1c48c78bd9e00017763f4d3bbc
kimhanlim97 <kimhanlim97@gmail.com>
2022-04-21 12:57:29 +0900

    각 코드의 중복되는 부분을 미들웨어를 통해 줄임

--- node.mjs
+++ node.mjs
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { request } from 'express'
 import compression from 'compression'
 
+import db from './lib/db.js'
 import { home, page } from './lib/topic.js'
 import { create, createProcess } from './lib/create.js'
 import { update, updateProcess } from './lib/update.js'
@@ -12,33 +13,43 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(compression());
+app.get('*', (req, res, next) => {
+  db.query("SELECT * FROM topic", (err, topicList) => {
+    if (err) {
+      throw err
+    }
+    req.topicList = topicList;
+    next()
+  })
+})
 
 app.get('/', (req, res) => {
-  home(res);
+  home(req, res);
 });
 
 app.get('/page/:pageId', (req, res) => {
-  page(res, req.params)
+  console.log(req.topicList, req.params);
+  page(req, res);
 });
 
 app.get('/create', (req, res) => {
-  create(res)
+  create(req, res)
 })
 
 app.post('/create', (req, res) => {
-  createProcess(res, req.body);
+  createProcess(req, res);
 })
 
 app.get('/update/:pageId', (req, res) => {
-  update(res, req.params);
+  update(req, res);
 })
 
 app.post('/update', (req, res) => {
-  updateProcess(res, req.body);
+  updateProcess(req, res);
 })
 
 app.post('/delete', (req, res) => {
-  deleteProcess(res, req.body)
+  deleteProcess(req, res);
 })
 
 app.listen(port, () => {
